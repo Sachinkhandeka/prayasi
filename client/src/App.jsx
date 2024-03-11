89ea982cddbd65a10eeed1117324387d936c6807
Sachin Khandeka <khandekasachin271@gmail.com>
2024-03-11 16:06:07 +0530

    created header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { Suspense } from "react";
 import { BrowserRouter,Routes, Route } from "react-router-dom"
+
+// pages import
 const Home = React.lazy(()=> import("./pages/Home"));
 const About = React.lazy(()=> import("./pages/About"));
 const Signin = React.lazy(()=> import("./pages/Signin"));
@@ -8,18 +10,22 @@ const Signup = React.lazy(()=> import("./pages/Signup"));
 const Dashboard = React.lazy(()=> import("./pages/Dashboard"));
 const Projects = React.lazy(()=> import("./pages/Projects"));
 
+//components import 
+import Header from "./components/Header";
+
 export default function App() {
   return (
     <>
     <BrowserRouter>
-       <Routes>
+      <Header /> 
+      <Routes>
         <Route path="/" element={ <Suspense fallback={<div>Loading ...</div>}><Home /></Suspense> }/>
         <Route path="/about" element={ <Suspense fallback={<div>Loading ...</div>}> <About /> </Suspense> }/>
         <Route path="/signup" element={ <Suspense fallback={<div>Loading ...</div>} ><Signup /></Suspense> }/>
         <Route path="/signin" element={ <Suspense fallback={<div>Loading ...</div>} ><Signin /></Suspense> }/>
         <Route path="/dashboard" element={ <Suspense fallback={<div>Loading ...</div>} ><Dashboard /></Suspense> }/>
         <Route path="/projects" element={ <Suspense fallback={<div>Loading ...</div>}><Projects /></Suspense> }/>
-       </Routes>
+      </Routes>
     </BrowserRouter>
     </>
   )
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiFillSafetyCertificate, AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+
+export default function Header() {
+    const location = useLocation();
+    return(
+        <>
+        <Navbar className="border border-b-2" >
+            <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"  >
+                <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">Prayasi</span>
+                Blog
+            </Link>
+            <form>
+                <TextInput type="text" placeholder="Search..." rightIcon={ AiOutlineSearch } className="hidden md:block" />
+            </form>
+            <Button className="md:hidden " color="gray" pill>
+                <AiOutlineSearch />
+            </Button>
+            <div className="flex items-center gap-4 md:order-2 " >
+                <Button className="w-12 h-10 hidden md:block" color="gray" pill > 
+                    <FaMoon />
+                </Button>
+                <Link to="/signin" >
+                    <Button gradientDuoTone={"purpleToBlue"}>Sign in</Button>
+                </Link>
+                <Navbar.Toggle />
+            </div>
+            <Navbar.Collapse >
+                <Navbar.Link active={ location.pathname === "/" } as={"div"} >
+                    <Link to="/">Home</Link>
+                </Navbar.Link>
+                <Navbar.Link active={ location.pathname === "/about" } as={"div"} >
+                    <Link to="/about">About</Link>
+                </Navbar.Link>
+                <Navbar.Link active={ location.pathname === "/projects" } as={"div"} >
+                    <Link to="/projects">Projects</Link>
+                </Navbar.Link>
+            </Navbar.Collapse>
+        </Navbar>
+        </>
+    )
+}
