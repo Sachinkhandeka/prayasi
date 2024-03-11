298b0e515a877cf8007d6f2a42834972db1d367c
Sachin Khandeka <khandekasachin271@gmail.com>
2024-03-11 17:24:36 +0530

    created server side folder and files

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -23,7 +23,7 @@ export default function Header() {
                     <FaMoon />
                 </Button>
                 <Link to="/signin" >
-                    <Button gradientDuoTone={"purpleToBlue"}>Sign in</Button>
+                    <Button gradientDuoTone={"purpleToBlue"} outline>Sign in</Button>
                 </Link>
                 <Navbar.Toggle />
             </div>
