cbf9d59b653617c5ef795194212105dc4fadafea
Sachin Khandeka <khandekasachin271@gmail.com>
2024-03-11 23:40:03 +0530

    create server side folder

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
-if( process.env.NODE_ENV !== "production" ) {
+if(process.env.NODE_ENV !== "production") {
     require("dotenv").config();
 }
 const express = require("express");
-const port = 8080 ; 
+const port =  8080 ; 
 const app = express();
 const mongoose = require("mongoose");
 const DB_URL = process.env.MONGO_DB_URL ; 
 
 main().then(()=> {
-    console.log("connected to mongodb url successfully!");
+    console.log("Server Connected with DB successfully!");
 }).catch((err)=> {
     console.log(err);
 });
 
 async function main() {
-    await mongoose.connect(DB_URL) ;
+   await  mongoose.connect(DB_URL);
 }
+
 app.listen(port , ()=> {
-    console.log(`server is running on ${port}`);
+    console.log(`server is  running on port ${port}`);
 });
