5d30ee238f2df27c04f4dfa1adff60ff4095f93a
Sachin Khandeka <khandekasachin271@gmail.com>
2024-03-11 18:07:15 +0530

    establishment of mongoDB connection code

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
+if( process.env.NODE_ENV !== "production" ) {
+    require("dotenv").config();
+}
 const express = require("express");
 const port = 8080 ; 
 const app = express();
+const mongoose = require("mongoose");
+const DB_URL = process.env.MONGO_DB_URL ; 
 
+main().then(()=> {
+    console.log("connected to mongodb url successfully!");
+}).catch((err)=> {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(DB_URL) ;
+}
 app.listen(port , ()=> {
     console.log(`server is running on ${port}`);
 });
