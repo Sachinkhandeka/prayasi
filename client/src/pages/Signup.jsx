0414f38bc0be4ce2eb7a14bd72d6987f085c1695
Sachin Khandeka <khandekasachin271@gmail.com>
2024-03-12 22:01:36 +0530

    update in singup UI

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom";
 export  default function Signup() {
     return(
         <>
-        <div className="min-h-screen mt-4 flex flex-col md:flex-row justify-around items-center px-4 md:px-10">
+        <div className="min-h-screen mt-4 flex flex-col lg:flex-row justify-around items-center px-4 lg:px-10">
             <div className="px-4 py-2 max-w-xl " >
-                <h1 className=" text-4xl md:text-[4rem] font-bold underline whitespace-nowrap mb-[4rem]" >Welcome to</h1>
+                <h1 className=" text-4xl md:text-[4rem] font-bold underline whitespace-nowrap mb-[2rem]" >Welcome to</h1>
                 <Brand className={"font-bold dark:text-white text-xl md:text-4xl"} />
                 <p className=" mt-4 md:mt-[2.5rem] text-gray-500 leading-8 text-lg" >Where your words find a home and your stories come to life. Start your journey of expression and connection today!</p>
             </div>
-            <div>
-                <div className="border border-gray-200 px-8 py-4 rounded-sm shadow-lg" >
+            <div className="w-full lg:w-auto px-4 py-2 max-w-xl" >
+                <div className="border border-gray-200 px-8 py-4 rounded-sm shadow-lg w-full" >
                 <form>
                     <div className="mt-6" >
                        <Label value="Your username" />
@@ -31,7 +31,7 @@ export  default function Signup() {
                               id="firstName"
                             />
                         </div>
-                        <div>
+                        <div className="mt-6 md:mt-0" >
                             <Label value="Your lastName" />
                             <TextInput 
                                type="text"
